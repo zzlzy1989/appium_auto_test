52c9bf8e95425f6e4e4d27bef28873487822f95e
Gao Xu <394845369@qq.com>
2019-06-27 23:46:23 +0800

    commit README.md

--- Demo/app_test1.py
+++ Demo/app_test1.py
@@ -6,20 +6,26 @@
 # Project   : appium_auto_test
 # @Software : PyCharm
 
+from appium import webdriver
 
 # 操作系统、系统版本、app应用
 desired_caps = {}
 desired_caps["platformName"]="Android"
-desired_caps["platformVersion"]="8.1"
+desired_caps["platformVersion"]="5.1.1"
 desired_caps["deviceName"]="Android Emulator"
 
 
 # app应用：包名---唯一标识
 # 如何得到app的包名？ apk包
-
-
-
-
-
 # 获取应用包名和入口activity
 # 使用命令  aapt dump badging apk应用名
+
+desired_caps["appPackage"]=""
+desired_caps["appActivity"]=""
+
+# 2、与appium建立连接
+
+webdriver.Remote("http://127.0.0.1:4723/wd/hub",desired_caps)
+
+# 3、设备在线:连接上USB，开启手机上面的USB调试模式。开发者选项
+# 在电脑上确认能够识别的设备：adb devices
